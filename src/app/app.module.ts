6dd94a6810fd2b5152d8cffafebbc4b9119add66
Damilola <omifaredammy@gmail.com>
2019-11-24 23:21:24 +0000

    add login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { LoginComponent } from './login/login.component';
 
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent }];
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes,
+       {enableTracing: true } )// for debugging only.
+],
   providers: [],
   bootstrap: [AppComponent]
 })
